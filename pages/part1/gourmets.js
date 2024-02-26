7aa0aa6120b6df0d428092fd63edfc0b29aa4e16
rui-toume <rui.amata.36@gmail.com>
2024-02-26 17:13:57 +0900

    デバックのため、console.logの追加

--- pages/part1/gourmets.js
+++ pages/part1/gourmets.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import getConfig from 'next/config';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
 
 const fetchData = async (keyword) => {
   const { API_HOST } = getConfig().publicRuntimeConfig;
-
   const query = new URLSearchParams();
   if (keyword) query.set('keyword', keyword);
-
   const host = process.browser ? '' : API_HOST;
   const res = await fetch(`${host}/api/shops?${query.toString()}`);
   return await res.json();
@@ -14,22 +21,57 @@ const fetchData = async (keyword) => {
 
 const Shops = ({ shops }) => {
   return (
-    <ul>
-      {shops.map((shop) => {
-        return <li key={shop.id}>{shop.name}</li>;
-      })}
-    </ul>
+    <Container component="main" maxWidth="md">
+      <Box
+        component="form"
+        noValidate
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <List>
+          {shops.map((shop) => {
+            return (
+              <ListItem key={shop.id}>
+                <ListItemButton
+                  onClick={() => {
+                    // TODO: goto shop detail
+                  }}
+                >
+                  <ListItemAvatar>
+                    <Avatar alt={shop.name} src={shop.logo_image} />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={`${shop.genre.name} ${shop.name}`}
+                    secondary={
+                      <>
+                        <Typography variant="body1" component="span">
+                          {`${shop.catch} ${shop.shop_detail_memo}`}
+                        </Typography>
+                        <Typography variant="caption">{shop.address}</Typography>
+                      </>
+                    }
+                  />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Box>
+    </Container>
   );
 };
 
 export const getServerSideProps = async (req) => {
+  console.log(req.query.keyword);
   const data = await fetchData(req.query.keyword);
-
   return {
     props: {
       shops: data,
     },
   };
 };
-
 export default Shops;
